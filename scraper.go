c070f608426ee52012afc9c04f42ab0aadeb2ead
72 <72sevenzy2@gmail.com>
2026-08-18 17:48:22 +0500

    feat: add initial constructor

--- scraper.go
+++ scraper.go
@@ -1,3 +1,26 @@
 package scraper
 
+import (
+	"fmt"
+	"sync"
+)
 
+type Crawler struct {
+	depth int
+
+	mu      sync.Mutex
+	origins map[string]bool
+}
+
+func NewCrawler() *Crawler {
+	return &Crawler{
+		origins: make(map[string]bool),
+	}
+}
+
+func (c *Crawler) Crawl(url string, depth int) {
+	if depth > c.depth {
+		fmt.Println("max depth exceeded.")
+		return
+	}
+}
