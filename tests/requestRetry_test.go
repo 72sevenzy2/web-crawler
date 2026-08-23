44c7d8bec8e027157826793dd0a589ed496954b3
72 <72sevenzy2@gmail.com>
2026-08-23 14:12:30 +0500

    refactor: update retry test regarding new http.RoundTrip change.

--- tests/requestRetry_test.go
+++ tests/requestRetry_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/72sevenzy2/web-crawler"
 )
@@ -15,17 +16,27 @@ func TestRequestWithRetry(t *testing.T) {
 		attempts++
 		if attempts < 3 {
 			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
 		w.WriteHeader(http.StatusOK)
 	}))
 	defer server.Close()
 
-	client := http.Client{}
-	resp, err := crawler.RequestWithRetry(context.Background(), server.URL, 5, &client)
+	client := crawler.NewRetryClient(time.Millisecond * 100, 10)
+
+
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, server.URL, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer resp.Body.Close()
 
 	if attempts != 3 {
 		t.Fatalf("expected %d attempts, got %d", 3, attempts)
