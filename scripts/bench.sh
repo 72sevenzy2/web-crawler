8414487061dcc3a030bd008bcce16f64c72480fa
72 <72sevenzy2@gmail.com>
2026-09-02 18:02:09 +0500

    test: display bytes per op and allocs with -benchmem.

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -3,7 +3,7 @@
 cd .. || exit 1
 cd benchmarks || exit 1
 
-go test -bench BenchmarkCrawling -count 10 -memprofile=mem.prof > results.txt
+go test -bench BenchmarkCrawling -benchmem -count 10 -memprofile=mem.prof > results.txt
 benchstat results.txt
 
 # open memory profiling afterwards
