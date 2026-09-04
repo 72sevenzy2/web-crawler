c32bbf8f79f93e2abbb89a74dea381ebe2414bfb
72 <72sevenzy2@gmail.com>
2026-09-04 22:05:01 +0500

    refactor: add initial crawling command.

--- cli/main.go
+++ cli/main.go
@@ -2,20 +2,36 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"os"
+	"strings"
 
-	//"github.com/72sevenzy2/web-crawler"
+	"github.com/72sevenzy2/web-crawler"
 )
 
 func main() {
-	fmt.Println("test")
+	c := crawler.NewCrawler(10, true, 5)
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		fmt.Println("aa")
+		fmt.Print(">")
 		safe := scanner.Scan()
 		if !safe {
-			fmt.Println("not safe")
+			if err := scanner.Err(); err != nil {
+				fmt.Println("err:", err.Error())
+			}
+			break
+		}
+		parts := strings.Fields(scanner.Text())
+
+		switch strings.ToLower(parts[0]) {
+		case "crawl":
+			if len(parts) < 2 {
+				fmt.Println("include a url to crawl through.")
+				continue
+			}
+
+			c.Start(context.Background(), parts[1])
 		}
 	}
 }
